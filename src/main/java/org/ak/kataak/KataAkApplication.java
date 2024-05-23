ef32cd2812de9cdbcdda2bf8ba3381eb2488e430
meriam <meriam.fakhi@gmail.com>
2024-05-24 00:42:24 +0200

    init commit

---
+++ src/main/java/org/ak/kataak/KataAkApplication.java
@@ -0,0 +1,13 @@
+package org.ak.kataak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataAkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataAkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ak/kataak/KataAkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ak.kataak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataAkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
